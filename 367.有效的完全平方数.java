dcde49d0c564377cd5625d3f227de1ea30dea7ec
pengzhisheng <pzs034@gmail.com>
2023-04-03 14:24:03 +0800

    add 367

---
+++ 367.有效的完全平方数.java
@@ -0,0 +1,30 @@
+import javax.lang.model.element.Element;
+
+/*
+ * @lc app=leetcode.cn id=367 lang=java
+ *
+ * [367] 有效的完全平方数
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 0;
+        int right = num;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            long x = (long)mid * mid;
+            if (x < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left * left == num;
+    }
+}
+// @lc code=end
+
